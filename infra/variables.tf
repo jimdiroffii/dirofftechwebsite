979e9a68e8e40a6542e868a218f619c78f5168b5
Jim Diroff II <jim@dirofftech.com>
2025-08-09 09:40:44 -0400

    Fix tf variable, update gitignore for tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,5 @@
-variable aws_region {
-	description = "AWS Region for all resources"
-	type = string
-	default = "us-east-2"
+variable "aws_region" {
+  description = "AWS Region for all resources"
+  type        = string
+  default     = "us-east-2"
 }
